3cf337325bd2a221098a1764d8ca534012e0b904
Muhammad Manan <muhammadmanan7175@gmail.com>
2022-07-03 18:44:23 +0500

    Create class.cpp

---
+++ class.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class Person {
+	string name = "Max";
+	    int age = 30;
+	    void greet() {
+	    	print('Hi, I am ' + name + ' and I am ' + age.toString() + ' years old!';
+	    }
+	};
+
+int main() {
+	var myself = Person();
+	print(myself.name);
+	
+}
