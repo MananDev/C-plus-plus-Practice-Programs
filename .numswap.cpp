9791eb990612ee9cef7fc2076a76f0af212a794d
Muhammad Manan <muhammadmanan7175@gmail.com>
2022-07-03 18:38:01 +0500

    Create .numswap.cpp

---
+++ .numswap.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num1=5;
+	int num2=10;
+	 num1= num1+5;
+	 num2= num2-5;
+	 cout<< "Values After Swapping" << endl;
+	 cout<< "value 1:" << num1 << endl;
+	 cout<< "value 2:" << num2 << endl;
+	return 0;
+}
