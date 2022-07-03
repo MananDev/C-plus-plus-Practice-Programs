ae067a8c04dfc34571169e1ede3252666516d571
Muhammad Manan <muhammadmanan7175@gmail.com>
2022-07-03 18:38:17 +0500

    Create alloperations.cpp

---
+++ alloperations.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=5;
+    int b=9;
+    cout<< "Addition Of numbers is:" << a+b << endl;
+    cout<< "Subtraction Of numbers is:" << a-b << endl;
+    cout<< "Division Of numbers is:" << a/b << endl;
+    cout<< "Multiplication Of numbers is:" << a*b << endl;
+	return 0;
+}
