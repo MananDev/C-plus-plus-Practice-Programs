7d39fbca123b616fad440df5eff78b0e53953a3f
Muhammad Manan <muhammadmanan7175@gmail.com>
2022-07-03 18:44:37 +0500

    Create inputandsum.cpp

---
+++ inputandsum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y;
+    int sum;
+    cout<< "Enter First Number:";
+    cin>>x;
+    cout<< "Enter Second Number:";
+    cin>>y;
+    sum=x+y;
+    cout<< "Sum of 2 numbers is=" << sum;
+	return 0;
+}
