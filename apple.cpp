3e8940d4139338d2a0afb53212d8d65e0f463edd
Muhammad Manan <muhammadmanan7175@gmail.com>
2022-07-03 18:38:26 +0500

    Create apple.cpp

---
+++ apple.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[5] = {1,2,3,4,5} , b[5] = {4,6,8,10,12};
+	for(int i=0; i<=5;i++)
+	{
+		a[i]=a[i]+b[i];
+		cout<<a[i]<<endl;
+	}
+}
+	
+	
+	
+	
+	
+	
+	
