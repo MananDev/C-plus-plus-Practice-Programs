f86800a381fa5c57f2be419404c331c82a1e6a45
Muhammad Manan <muhammadmanan7175@gmail.com>
2022-07-03 18:46:17 +0500

    Create multiplyfloat.cpp

---
+++ multiplyfloat.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float x,y;
+    float mul;
+    cout<<"Enter 1st Floating Value";
+    cin>>x;
+    cout<<"Enter 2nd Floating Value";
+    cin>>y;
+    mul= x*y;
+    cout<< "Answer of float multiplication is=" << mul;
+	return 0;
+}
